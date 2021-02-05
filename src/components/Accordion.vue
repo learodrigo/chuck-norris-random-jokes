1a1e24ff75ff0dc425af2eced306d7d79521beab
Leandro Rodrigo <leandro.rodrigo@zalando.de>
2021-02-05 12:22:12 +0100

    Initial setup and dummy component

---
+++ src/components/Accordion.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>{{ item.title }}</h3>
+        <h4 @click="toggle = !toggle ">Details</h4>
+        <p v-if="toggle">{{ item.description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Accordion',
+    props: ['item'],
+    data: () => {
+        return {
+            toggle: false
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
